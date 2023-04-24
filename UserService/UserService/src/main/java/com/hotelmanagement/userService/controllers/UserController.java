54a3a388bbaf550a5b53b7295d9bfe4366bd97ae
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2023-04-24 17:36:59 +0530

    Rate Limiter config added to User Service

--- UserService/UserService/src/main/java/com/hotelmanagement/userService/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/hotelmanagement/userService/controllers/UserController.java
@@ -18,6 +18,7 @@ import com.hotelmanagement.userService.entities.User;
 import com.hotelmanagement.userService.services.UserService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -41,7 +42,8 @@ public class UserController {
 	// single user get
 	@GetMapping("/{userId}")
 //	@CircuitBreaker(name="ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
-	@Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+//	@Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+	@RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
 	public ResponseEntity<User> getSingleUser(@PathVariable String userId){
 		logger.info("Get Single User Handler: Usercontroller");
 		logger.info("Retry count: {}", retryCount);
