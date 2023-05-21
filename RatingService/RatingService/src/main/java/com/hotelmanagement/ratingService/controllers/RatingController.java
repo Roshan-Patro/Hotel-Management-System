7e8a56d39732332ba3c1529c0f59ef28f4d5bf59
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2023-05-21 14:00:52 +0530

    spring security implemented in hotel service

--- RatingService/RatingService/src/main/java/com/hotelmanagement/ratingService/controllers/RatingController.java
+++ RatingService/RatingService/src/main/java/com/hotelmanagement/ratingService/controllers/RatingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ public class RatingController {
 	@Autowired
 	private RatingServices ratingServices;
 	
+	@PreAuthorize("hasAuthority('Admin')")
 	@PostMapping
 	public ResponseEntity<Rating> createRating(@RequestBody Rating rating){
 		return ResponseEntity.status(HttpStatus.CREATED).body(ratingServices.createRating(rating));
@@ -32,6 +34,7 @@ public class RatingController {
 		return ResponseEntity.ok(ratingServices.getRatingById(ratingId));
 	}
 	
+	@PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
 	@GetMapping("/user/{userId}")
 	public ResponseEntity<List<Rating>> getRatingsByUserId(@PathVariable String userId){
 		return ResponseEntity.ok(ratingServices.getAllByUserId(userId));
